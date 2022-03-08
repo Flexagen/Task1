9fd12e49d3d2219f98ae2f6cae813d263e9853ca
Maksim Pavlov <99464484+Flexagen@users.noreply.github.com>
2022-03-08 11:45:50 +0300

    Add files via upload

---
+++ app/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "equation.h"
+
+int main()
+{
+    double a, b, c, d, x1, x2;
+    x1 = 0;
+    x2 = 0;
+    a = 10;
+    b = 4;
+    c = -100;
+    d = b * b - 4 * a * c;
+
+    int rezult = equation(&a, &b, &c, &d,  &x1, &x2);
+    if (rezult == 1)
+        printf("x1 = %lf; x2 = %lf\n", x1, x2);
+    if (rezult == 0)
+        printf("x1 = x2 = %lf\n", x1);
+    if (rezult == -1)
+        printf("Корней не существует.");
+    return 0;
+}
