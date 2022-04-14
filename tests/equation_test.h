d3b81497c71808e4d239c6bfa04a3794042682c0
Maksim Pavlov <mpavlov@cs.petrsu.ru>
2022-04-14 10:58:35 +0300

    commit

--- tests/equation_test.h
+++ tests/equation_test.h
@@ -16,70 +16,51 @@ TEST(Test1, equation_Positive_Test) {
     double x2 = 0;
 
     EXPECT_EQ(equation(10, 22.5, -56, &x1, &x2), 1);
-    if (x1 != 0.0 && x2 != 0.0)
-        SUCCEED();
-    else
-        FAIL();
+    ASSERT_FLOAT_EQ (float(x1), float(1.495234));
+    ASSERT_FLOAT_EQ (float(x2), float(-3.745234));
 
     x1 = 0;
     x2 = 0;
 
     EXPECT_EQ(equation(0, 8, -16, &x1, &x2), 0);
-
-    if (x1 == 2.0 && x2 == 0.0)
-        SUCCEED();
-    else
-        FAIL();
+    ASSERT_FLOAT_EQ (float(x1), float(2));
+    ASSERT_FLOAT_EQ (float(x2), float(0));
 
     x1 = 0;
     x2 = 0;
 
     EXPECT_EQ(equation(3, 6, 3, &x1, &x2), 0);
-
-    if (x1 == -9.0 && x2 == 0.0)
-        SUCCEED();
-    else
-        FAIL();
+    ASSERT_FLOAT_EQ (float(x1), float(-9));
+    ASSERT_FLOAT_EQ (float(x2), float(0));
 
     x1 = 0;
     x2 = 0;
 
     EXPECT_EQ(equation(0, 0, 27, &x1, &x2), -1);
-
-    if (x1 == x2 && x2 == 0.0)
-        SUCCEED();
-    else
-        FAIL();
+    ASSERT_FLOAT_EQ (float(x1), float(0));
+    ASSERT_FLOAT_EQ (float(x2), float(0));
 
     x1 = 0;
     x2 = 0;
 
     EXPECT_EQ(equation(10, 22.5, 0, &x1, &x2), 1);
-
-    if (x1 == 0.0 && x2 != 0.0)
-        SUCCEED();
-    else
-        FAIL();
+    ASSERT_FLOAT_EQ (float(x1), float(0));
+    ASSERT_FLOAT_EQ (float(x2), float(-2.25));
 
     x1 = 0;
     x2 = 0;
 
     EXPECT_EQ(equation(25, 0, -625, &x1, &x2), 1);
-
-    if (x1 == -x2 && x2 != 0.0)
-        SUCCEED();
-    else
-        FAIL();
+    ASSERT_FLOAT_EQ (float(x1), float(5));
+    ASSERT_FLOAT_EQ (float(x2), float(-5));
 
     x1 = 0;
     x2 = 0;
 
     EXPECT_EQ(equation(142.5, 0, 0, &x1, &x2), 0);
+    ASSERT_FLOAT_EQ (float(x1), float(0));
+    ASSERT_FLOAT_EQ (float(x2), float(0));
 
-    if (x1 == x2 && x2 == 0.0)
-        SUCCEED();
-    else
-        FAIL();
 }
 
 TEST(Test2, equation_Positive_Test) {
@@ -87,21 +68,15 @@ TEST(Test2, equation_Positive_Test) {
     double x2 = 0;
 
     EXPECT_EQ(equation(0, 8, -16, &x1, &x2), 0);
-
-    if (x1 == 2.0 && x2 == 0.0)
-        SUCCEED();
-    else
-        FAIL();
+    ASSERT_FLOAT_EQ (float(x1), float(2));
+    ASSERT_FLOAT_EQ (float(x2), float(0));
 
     x1 = 0;
     x2 = 0;
 
     EXPECT_EQ(equation(0, -0.25, 0, &x1, &x2), 0);
-
-    if (x1 == x2 && x1 == 0.0)
-        SUCCEED();
-    else
-        FAIL();
+    ASSERT_FLOAT_EQ (float(x1), float(0));
+    ASSERT_FLOAT_EQ (float(x2), float(0));
 }
 
 TEST(Test3, equation_Negative_Test) {
@@ -109,11 +84,8 @@ TEST(Test3, equation_Negative_Test) {
     double x2 = 0;
 
     EXPECT_EQ(equation(25, 0, 625, &x1, &x2), -1);
-
-    if (x1 == x2 && x2 == 0.0)
-        SUCCEED();
-    else
-        FAIL();
+    ASSERT_FLOAT_EQ (float(x1), float(0));
+    ASSERT_FLOAT_EQ (float(x2), float(0));
 }
 
 
@@ -122,11 +94,8 @@ TEST(Test4, equation_Negative_Test) {
     double x2 = 0;
 
     EXPECT_EQ(equation(0, -0, 0.0, &x1, &x2), -2);
-
-    if (x1 == x2 && x2 == 0.0)
-        SUCCEED();
-    else
-        FAIL();
+    ASSERT_FLOAT_EQ (float(x1), float(0));
+    ASSERT_FLOAT_EQ (float(x2), float(0));
 }
 
 #endif // EQUATION_TEST_H
